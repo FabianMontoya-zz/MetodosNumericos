ed98104d6f59e3c3434fdf2fcfe1b76ef53daf24
Fabian Montoya <nachofdm@hotmail.com>
2017-08-16 18:32:29 -0500

    Commit Inicial
    Commit Inicial

---
+++ MetodosNumericos1/MetodosNumericos1/Funcion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Funcion
+{
+    /// <summary>
+    /// Ejecuta la función 3.2(x)^2 + 2.7(x) - 1.3 ; recibe valor para X y retorna resultado
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static double EjecutarFuncion(double x)
+    {
+        double result = 0;
+        result = 3.2 * (Math.Pow(x, 2)) + 2.7 * (x) - 1.3;
+        return result;
+    }
+
+    public static double ObtenerX2(double x0, double x1)
+    {
+        double result = 0;
+        result = (x0 + x1) / 2;
+        return result;
+    }
+}
+
---
+++ MetodosNumericos1/MetodosNumericos1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosNumericos1
+{
+    class Program
+    {
+        Funcion funciones = new Funcion();
+        static void Main(string[] args)
+        {
+
+            double x0 = 0;
+            double x1 = 4;
+            double x2 = 0;
+
+            double fx0 = 0;
+            double fx1 = 0;
+            double fx2 = 0;
+            //Console.WriteLine("Rango inicial para X0");
+            //x0 = Double.Parse(Console.ReadLine());
+            int Maxloops = 50;
+            int loops = 0;
+
+            while (loops <= Maxloops)
+            {
+                fx0 = Funcion.EjecutarFuncion(x0);
+                fx1 = Funcion.EjecutarFuncion(x1);
+                x2 = Funcion.ObtenerX2(x0, x1);
+                fx2 = Funcion.EjecutarFuncion(x2);
+
+                if()
+
+                loops++;
+            }
+        }
+
+        
+    }
+}
