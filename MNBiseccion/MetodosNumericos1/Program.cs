7b5320830dd3fd55ccd3f6bd83fcde7a700431cb
Fabian Montoya <nachofdm@hotmail.com>
2017-08-19 13:09:18 -0500

    MN: Bisección

--- MNBiseccion/MetodosNumericos1/Program.cs
+++ MNBiseccion/MetodosNumericos1/Program.cs
@@ -15,13 +15,14 @@ namespace MetodosNumericos1
             double x0 = 0;
             double x1 = 4;
             double x2 = 0;
+            double x2Old = 0;
 
             double fx0 = 0;
             double fx1 = 0;
             double fx2 = 0;
             //Console.WriteLine("Rango inicial para X0");
             //x0 = Double.Parse(Console.ReadLine());
-            int Maxloops = 50;
+            int Maxloops = 200;
             int loops = 0;
 
             while (loops <= Maxloops)
@@ -30,13 +31,45 @@ namespace MetodosNumericos1
                 fx1 = Funcion.EjecutarFuncion(x1);
                 x2 = Funcion.ObtenerX2(x0, x1);
                 fx2 = Funcion.EjecutarFuncion(x2);
+                Console.WriteLine("lop = " + loops + ") -- X0 =" + x0 + " ; X1 = " + x1 + " ; f(x0) =" + fx0 + " ; f(x1) =" + fx1 + ";");
+                Console.WriteLine("x2 = " + x2 + " ; f(x2) = " + fx2);
+                if (loops > 0)
+                {
+                    string abs = "" + Math.Abs(x2 - x2Old);
+                    Console.WriteLine("Absoluto = " + abs);
+                    int tam = abs.Length;
+                    try
+                    {
+                        string notacion = abs.Substring((tam - 4), 4);
+                        if (notacion.ToUpper().Contains("E-11"))
+                        {
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("** La raiz es: " + x2);
+                            break;
+                        }
+                    }
+                    catch { }
+                }
+                if (fx0 * fx2 >= 0)
+                {
+                    x0 = x2;
+                    Console.WriteLine("**Cambiamos x0 por x2");
 
-                if()
+                }
+                else
+                {
+                    x1 = x2;
 
+                    Console.WriteLine("**Cambiamos x1 por x2");
+                }
+                x2Old = x2;
                 loops++;
             }
+
+            Console.ReadLine();
         }
 
-        
+
     }
 }
