06589ce0ac85a695b5048fc7f0e5ac0f58345a68
Fabian Montoya <nachofdm@hotmail.com>
2017-08-19 12:54:48 -0500

    MN: Métodos de BIsección y Secante
    Se completan aplicativos con metodos de bisección y secante

---
+++ MNSecante/MNSecante/Funcion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Funcion
+{
+    /// <summary>
+    /// Ejecuta la función 3.2(x)^2 + 2.7(x) - 1.3 ; recibe valor para X y retorna resultado
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static double EjecutarFuncion(double x)
+    {
+        double result = 0;
+        result = 3.2 * (Math.Pow(x, 2)) + 2.7 * (x) - 1.3;
+        return result;
+    }
+    /// <summary>
+    /// Obtiene la pendiente
+    /// </summary>
+    /// <param name="x0"></param>
+    /// <param name="x1"></param>
+    /// <param name="fx0"></param>
+    /// <param name="fx1"></param>
+    /// <returns></returns>
+    public static double ObtenerM(double x0, double x1, double fx0, double fx1)
+    {
+        double result = 0;
+        result = ((fx1) - (fx0)) / ((x1) - (x0));
+        return result;
+    }
+    /// <summary>
+    /// Obtiene el punto de corte
+    /// </summary>
+    /// <param name="x0"></param>
+    /// <param name="fx0"></param>
+    /// <param name="fx1"></param>
+    /// <returns></returns>
+    public static double ObtenerB(double x0, double fx0, double M)
+    {
+        double result = 0;
+        result = (fx0) - (M) * (x0);
+        return result;
+    }
+    public static double ObtenerX2(double M, double B)
+    {
+        double result = 0;
+        result = -(B) / (M);
+        return result;
+    }
+}
---
+++ MNSecante/MNSecante/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNSecante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x0 = 0;
+            double x1 = 4;
+            double x2 = 0;
+            double x2Old = 0;
+
+            double fx0 = 0;
+            double fx1 = 0;
+            double M = 0;
+            double B = 0;
+            //Console.WriteLine("Rango inicial para X0");
+            //x0 = Double.Parse(Console.ReadLine());
+            int Maxloops = 200;
+            int loops = 0;
+
+            while (loops <= Maxloops)
+            {
+                fx0 = Funcion.EjecutarFuncion(x0);
+                fx1 = Funcion.EjecutarFuncion(x1);
+                M = Funcion.ObtenerM(x0, x1, fx0, fx1);
+                B = Funcion.ObtenerB(x0, fx0, M);
+                x2 = Funcion.ObtenerX2(M, B);
+                Console.WriteLine("lop = " + loops + ") -- X0 =" + x0 + " ; X1 = " + x1 + " ; f(x0) =" + fx0 + " ; f(x1) =" + fx1 + ";");
+                Console.WriteLine("M = " + M + " ; B = " + B + " ; x2 = " + x2 + " ;");
+                if (loops > 0)
+                {
+                    string abs = "" + Math.Abs(x2 - x2Old);
+                    Console.WriteLine("Absoluto = " + abs);
+                    int tam = abs.Length;
+                    try
+                    {
+                        string notacion = abs.Substring((tam - 4), 4);
+                        if (notacion.ToUpper().Contains("E-11"))
+                        {
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("===========================================");
+                            Console.WriteLine("** La raiz es: " + x2);
+                            break;
+                        }
+                    }
+                    catch { }
+                }
+
+                x1 = x2;
+                x2Old = x2;
+                loops++;
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
