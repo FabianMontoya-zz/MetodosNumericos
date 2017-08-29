93f60c8a0a562d5cd60637c630e46a9893112164
Fabian Montoya <nachofdm@hotmail.com>
2017-08-28 21:57:49 -0500

    MN: Creción Programa Método de la derivada
    Se sube proyecto para calculo de la raiz con método de la derivada

---
+++ MNDerivada/MNDerivada/Clases/DiferencialClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DiferencialClass
+{
+    public double Resultado { get; set; }
+    public double Diferencial { get; set; }
+}
+
---
+++ MNDerivada/MNDerivada/Funcion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Funcion
+{
+    /// <summary>
+    /// Ejecuta la función F(x) = xe^x recibe valor para X y retorna resultado
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static double EjecutarFuncion1(double x)
+    {
+        double result = 0;
+        result = (x) * Math.Exp((x));
+        return result;
+    }
+    /// <summary>
+    /// Función que retorna el valor que ha de obtener H según un valor inicial obtenido
+    /// </summary>
+    /// <param name="h"></param>
+    /// <returns></returns>
+    public static double obtenerH(double h)
+    {
+        double result = 0;
+        result = (h) * Math.Pow(10, (-1));
+        return result;
+    }
+    /// <summary>
+    /// Función que devuelve la diferencial entre el resultado de la funciones F(x) y F(x+h) dividido en el valor de H
+    /// </summary>
+    /// <param name="Fx"></param>
+    /// <param name="fxh"></param>
+    /// <param name="h"></param>
+    /// <returns></returns>
+    public static double obtenerDiferencial(double Fx, double Fxh, double h)
+    {
+        double result = 0;
+        result = ((Fxh) - (Fx)) / (h);
+        return result;
+    }
+}
