c085248bb9ce3e8c8a6e7b620c4d86f323d76b3b
IanFelipe <elpronnunciante@gmail.com>
2017-08-30 09:02:26 -0500

    Parcial 2
    ok

--- PARCIAL/MNBiseccion/MetodosNumericos1/Funcion.cs
+++ PARCIAL/MNBiseccion/MetodosNumericos1/Funcion.cs
@@ -19,7 +19,7 @@ class Funcion
     {
 
         
-        double ejecutar = 0;
+        
 
 
         double x2 = 0;
@@ -35,7 +35,7 @@ class Funcion
         {
             fx0 = Funcion.EjecutarFuncion(x0);
             fx1 = Funcion.EjecutarFuncion(x1);
-            x2 = Funcion.ObtenerX2(x0, x1);
+            x2 = Funcion.ObtenerX2Bis(x0, x1);
             fx2 = Funcion.EjecutarFuncion(x2);
             Console.WriteLine("lop = " + loops + ") -- X0 =" + x0 + " ; X1 = " + x1 + " ; f(x0) =" + fx0 + " ; f(x1) =" + fx1 + ";");
             Console.WriteLine("x2 = " + x2 + " ; f(x2) = " + fx2);
@@ -96,7 +96,50 @@ class Funcion
         
     }
 
+    public static void Secante(double x0, double x1)
+    {
 
+        double x2 = 0;
+        double x2Old = 0;
+
+        double fx0 = 0;
+        double fx1 = 0;
+        double M = 0;
+        double B = 0;
+        //Console.WriteLine("Rango inicial para X0");
+        //x0 = Double.Parse(Console.ReadLine());
+        int Maxloops = 200;
+        int loops = 0;
+
+        while (loops <= Maxloops)
+        {
+            fx0 = Funcion.EjecutarFuncion(x0);
+            fx1 = Funcion.EjecutarFuncion(x1);
+            M = Funcion.ObtenerM(x0, x1, fx0, fx1);
+            B = Funcion.ObtenerB(x0, fx0, M);
+            x2 = Funcion.ObtenerX2Sec(M, B);
+            Console.WriteLine("lop = " + loops + ") -- X0 =" + x0 + " ; X1 = " + x1 + " ; f(x0) =" + fx0 + " ; f(x1) =" + fx1 + ";");
+            Console.WriteLine("M = " + M + " ; B = " + B + " ; x2 = " + x2 + " ;");
+            if (loops > 0)
+            {
+                double abs = Math.Abs(x2 - x2Old);
+
+                Console.WriteLine("Absoluto = " + abs);
+                //Console.WriteLine("Elevado: " + (xx * Math.Pow(10, 10)));
+                if ((abs * Math.Pow(10, 10)) < 1 && (abs * Math.Pow(10, 10)) > 0) //Llegamos a E-11
+                {
+                    Console.WriteLine("===========================================");
+                    Console.WriteLine("===========================================");
+                    Console.WriteLine("** La raiz es: " + x2);
+                    break;
+                }
+            }
+
+            x1 = x2;
+            x2Old = x2;
+            loops++;
+        }
+    }
     public static double EjecutarFuncion(double x)
     {
         double result = 0;
@@ -104,14 +147,32 @@ class Funcion
         return result;
     }
 
-    
-    public static double ObtenerX2(double x0, double x1)
+    //****************************************************************
+    public static double ObtenerX2Bis(double x0, double x1)
     {
         double result = 0;
         result = (x0 + x1) / 2;
         return result;
     }
+    public static double ObtenerM(double x0, double x1, double fx0, double fx1)
+    {
+        double result = 0;
+        result = ((fx1) - (fx0)) / ((x1) - (x0));
+        return result;
+    }
+    public static double ObtenerB(double x0, double fx0, double M)
+    {
+        double result = 0;
+        result = (fx0) - (M) * (x0);
+        return result;
+    }
 
+    public static double ObtenerX2Sec(double M, double B)
+    {
+        double result = 0;
+        result = -(B) / (M);
+        return result;
+    }
    
 }
 
--- PARCIAL/MNBiseccion/MetodosNumericos1/Program.cs
+++ PARCIAL/MNBiseccion/MetodosNumericos1/Program.cs
@@ -16,18 +16,23 @@ namespace MetodosNumericos1
             //PARCIAL PRIMER PUNTO, FABIAN FALTA HACER QUE EN EL PROCEDIMEINTO BISECCIÓN X2 SE GUARDE EN UNA MATRIZ Y QUE CADA VEZ QUE EL CICLO SE REPITA X1 O X0 AUMENTE 0.5 
             double x0 = 0;
             double x1 = 4;
-            int cont=0;
+            int contBis=0, contSec=0;
 
-            double ejecutar = 0;
+            
 
             do
             {
                 Funcion.Biseccion(x0, x1);
-                cont++;
-            } while (cont <= 6);
+                contBis++;
+            } while (contBis <= 5);
            
             //Console.WriteLine("Rango inicial para X0");
 
+            do
+            {
+                Funcion.Secante (x0, x1);
+                contSec++;
+            } while (contSec <= 14);
            
 
             Console.ReadLine();
