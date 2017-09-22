dab4131e00383bced652382f73774e9a6cff5602
Fabian Montoya <nachofdm@hotmail.com>
2017-09-21 22:07:43 -0500

    IN: Función para calculo de integrales
    Secompelta funcionalidad para realizar el calculo de las integrales según una función especifica

--- Integrales/Integrales/Funcion.cs
+++ Integrales/Integrales/Funcion.cs
@@ -5,14 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-    class Funcion
+class Funcion
+{
+    /// <summary>
+    /// Función encargada de ejecutar la operación matemática con base en una variable que recibe
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static double ejecutarFuncion(double x)
     {
-        public static double EjecutarFuncion(double x)
-        {
-            double result = 0;
-            result =(2 * x) -(Math.Cos(Math.Pow(x, 2)));
-            return result;
-        }
-
+        double result = 0;
+        result = (2 * x) - (Math.Cos(Math.Pow(x, 2)));
+        return result;
     }
+
+    /// <summary>
+    /// Función encargada de obtener el defecto de los resultados de las dos funciones
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <param name="fx0"></param>
+    /// <param name="fx1"></param>
+    /// <returns></returns>
+    public static double getDefecto(double delta, double fx0, double fx1)
+    {
+        double result = Math.Abs(fx0) <= Math.Abs(fx1) ? fx0 * delta : fx1 * delta;
+        return result;
+    }
+
+    /// <summary>
+    /// Función encargada de obtener el exceso de los resultados de las dos funciones
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <param name="fx0"></param>
+    /// <param name="fx1"></param>
+    /// <returns></returns>
+    public static double getExceso(double delta, double fx0, double fx1)
+    {
+        double result = Math.Abs(fx0) <= Math.Abs(fx1) ? fx1 * delta : fx0 * delta;
+        return result;
+    }
+
+    /// <summary>
+    /// Función que retorna el valor total del calculo del trapecio
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <param name="fx0"></param>
+    /// <param name="fx1"></param>
+    /// <returns></returns>
+    public static double getTrapecio(double delta, double fx0, double fx1)
+    {
+        double result = ((fx0 + fx1) * delta) / 2;
+        return result;
+    }
+
+}
 
--- Integrales/Integrales/Program.cs
+++ Integrales/Integrales/Program.cs
@@ -11,32 +11,51 @@ namespace Integrales
         static void Main(string[] args)
         {
 
-            double A = 1;
-            double B = 5;
-            double N = 10;
-            double DELTA = (B - A) / 2;
+            double A = -1; //Rango Inicial
+            double B = 0; //Rango final
+            double N = 10; //Cantidad de rectangulos
+            double DELTA = (B - A) / N;
 
-            double x0 = 0;
-            double x1 = 0;
-            double Fx0=0;
-            double Fx1=0;
-            double DEFEC=0;
-            double EXCES=0;
+            double x0 = A;
+            double x1 = x0 + DELTA;
+            double Fx0 = 0;
+            double Fx1 = 0;
+            double DEFEC = 0;
+            double EXCES = 0;
             double TRAP = 0;
 
             double TotalDEFEC = 0;
             double TotalEXCES = 0;
             double TotalTRAP = 0;
 
-
-            for (int i = 0; i <= N; i++)
+            //Comenzamos a realizar cada uno de los registros hasta alcanzar la cantidad requerida
+            for (int i = 1; i <= N; i++)
             {
-             
+                Console.WriteLine(i+")  x0: " + x0 + "  ||  x1:" + x1);
+                Fx0 = Funcion.ejecutarFuncion(x0);
+                Fx1 = Funcion.ejecutarFuncion(x1);
+                Console.WriteLine("Fx0: " + Fx0 + "  ||  Fx1:" + Fx1);
 
+                DEFEC = Funcion.getDefecto(DELTA, Fx0, Fx1);
+                EXCES = Funcion.getExceso(DELTA, Fx0, Fx1);
+                TRAP = Funcion.getTrapecio(DELTA, Fx0, Fx1);
+                Console.WriteLine("DEFEC: " + DEFEC + "  ||  EXCES:" + EXCES + "  ||  TRAP:" + TRAP);
 
+                TotalDEFEC = TotalDEFEC + DEFEC;
+                TotalEXCES = TotalEXCES + EXCES;
+                TotalTRAP = TotalTRAP + TRAP;
+
+                x0 = x1;
+                x1 = x0 + DELTA;
+                Console.WriteLine("====================================================================");
+                Console.WriteLine("====================================================================");
             }
-
-
+            Console.WriteLine("");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("*======================= RESULTADOS ===============================*");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("TotalDEFEC: " + TotalDEFEC + "  ||  TotalEXCES:" + TotalEXCES + "  ||  TotalTRAP:" + TotalTRAP);
+            Console.ReadLine();
         }
     }
 }
